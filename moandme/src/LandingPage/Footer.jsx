89e3c912370e23a41b484a64ada9d992b08c167b
AlexxanderP <AlexxanderP@users.noreply.github.com>
2019-10-28 23:01:08 -0700

    footer complete

--- moandme/src/LandingPage/Footer.jsx
+++ moandme/src/LandingPage/Footer.jsx
@@ -2,8 +2,16 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <div>
-      <div></div>
+    <div className="footer-container">
+      <div className="footer-links">
+        <h3> Home </h3>
+        <h3> About Us </h3>
+        <h3> Our Services </h3>
+        <h3> Contact Us </h3>
+      </div>
+      <div className="copyright">
+        <h4> Copyright 2019 Mo&Me Clutter Free - All Rights Reserved.</h4>
+      </div>
     </div>
   );
 };
