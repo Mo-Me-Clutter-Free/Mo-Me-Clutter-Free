282341fdd69c73b5a18da7a56da607ac797357a6
AlexxanderP <AlexxanderP@users.noreply.github.com>
2019-10-29 19:51:18 -0700

    meet mo and me page complete

--- moandme/src/AboutUs/AboutUs.jsx
+++ moandme/src/AboutUs/AboutUs.jsx
@@ -1,9 +1,56 @@
 import React from "react";
+import Nav from "../LandingPage/Navigation/Nav";
+import Footer from "../LandingPage/Footer";
+import MoandMe from "../imgs/MoandMe.png";
+import Jamie from "../imgs/Jamie.png";
+import Morgan from "../imgs/Morgan.png";
 
 const AboutUs = () => {
   return (
-    <div>
-      <div></div>
+    <div className="meet-container">
+      <Nav />
+      <h1 className="mo-and-me">Meet Mo & Me</h1>
+      <div className="who-we-are-container">
+        <img src={MoandMe} alt="moandme" />
+        <div className="who-we-are-inner-container">
+          <h2 className="who-we-are">Who We Are</h2>
+          <p className="who-we-are-p">
+            Mother and daughter team! I am the one that comes in and has the
+            plan of attack and sets the routine and new patterns for you. Morgan
+            is meticulous at details and fine tuning what I have set in place.
+            Together we have fun, get a lot done and make new friends along the
+            way.
+          </p>
+        </div>
+      </div>
+      <div className="jamie-container">
+        <div className="jamie-inner-container">
+          <h2 className="jamie">Jamie</h2>
+          <p className="jamie-p">
+            I started this business in Santa Clarita, CA in March 2018 after
+            hearing my whole life, “You’re so good at organizing you should
+            charge for it.” I am so passionate about helping others to live life
+            simpler. We don’t need to clutter our homes and lives with extra
+            “stuff”. Instead, enjoy life and the ones around you, and by having
+            a system in place for your home or office that you can maintain
+            easily you will have the extra time.
+          </p>
+        </div>
+        <img className="jamie-img" src={Jamie} alt="jamie-img" />
+      </div>
+      <div className="morgan-container">
+        <img className="morgan-img" src={Morgan} alt="morgan-img" />
+        <div className="morgan-inner-container">
+          <h2 className="morgan">Morgan</h2>
+          <p className="morgan-p">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Faucibus
+            pulvinar elementum integer enim neque volutpat ac. Ultrices sagittis
+            orci a scelerisque purus semper.
+          </p>
+        </div>
+      </div>
+      <Footer />
     </div>
   );
 };
